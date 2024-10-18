28accb5297156273a650b1ca67b17c854d7a1ed5
TrixterPro <trixindialimited19@gmail.com>
2024-10-18 21:29:46 +0530

    Release 1.0

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ class basicconfig:
     # Setting this to True will make it retrieve the API key from .env
     # Settings this to False will make it to retrieve the API key from the key.json file. (./utils/key.json)
 
-    use_env = True
+    use_env = False
 
 
     # NOTE: IF YOU HAVE use_env SET TO TRUE THEN YOU DON'T HAVE TO INPUT YOUR TOKEN HERE!
